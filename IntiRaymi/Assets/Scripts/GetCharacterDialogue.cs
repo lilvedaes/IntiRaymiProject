4fb1b114dc2b69a126d0dfac4592c436b8aae65a
Allerises <ibby.star.111@gmail.com>
2023-06-15 16:15:18 -0400

    spawning objects script
    "No character is interacting" error is back, no clue how or why it appears so ymmv

--- IntiRaymi/Assets/Scripts/GetCharacterDialogue.cs
+++ IntiRaymi/Assets/Scripts/GetCharacterDialogue.cs
@@ -10,10 +10,16 @@ namespace Inworld.Sample.UI
 {
     public class GetCharacterDialogue : MonoBehaviour
     {
+
+        public string[] keywords;
+        public GameObject[] spawnObjects;
         // Start is called before the first frame update
         void Start()
         {
-        
+            foreach (GameObject obj in spawnObjects)
+            {
+                obj.SetActive(false);
+            }
         }
 
         // Update is called once per frame
@@ -26,7 +32,13 @@ namespace Inworld.Sample.UI
         {
             if (input != "Player")
             {
-                Debug.Log(output);
+                int i = 0;
+                foreach (string word in keywords)
+                {
+                    if (output.Contains(word) && !spawnObjects[i].activeSelf)
+                        spawnObjects[i].SetActive(true);
+                    i++;
+                }
             }
         }
     }
