cfc147c4d95267d6289f992d9160c414ec078c17
Allerises <ibby.star.111@gmail.com>
2023-06-21 04:12:31 -0400

    Revert "Merge branch 'main' into LevelDesign"
    This reverts commit 76d9d55c8d71fb45aa6496aaf66f59714b03fc02, reversing
    changes made to 19ad38741a57679842078620b1b1dfff2da7023b.

--- IntiRaymi/Assets/Scripts/SetModelDirection.cs
+++ IntiRaymi/Assets/Scripts/SetModelDirection.cs
@@ -5,29 +5,16 @@ using UnityEngine;
 public class SetModelDirection : MonoBehaviour
 {
     private Vector3 positionLastFrame;
-    Animator m_Animator;
-
     // Start is called before the first frame update
     void Start()
     {
         positionLastFrame = transform.position;
-        m_Animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 movedVector = transform.position - positionLastFrame;
+        transform.forward = transform.position - positionLastFrame;
         positionLastFrame = transform.position;
-
-        if (movedVector.magnitude > 0.001f)
-        {
-            transform.forward = movedVector;
-            m_Animator.Play("Base Layer.Walking");
-        }
-        else
-        {
-            m_Animator.Play("Base Layer.Idle");
-        }
     }
 }
